411d9bb47fc1bc5edfb24a7e2477da7fe0c5c4a7
MihirMehta5695 <mihirm5695@gmail.com>
2016-11-01 16:41:01 +0530

    Creating the Stage and Scene in JavaFX for Task Killer"

---
+++ application/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+
+	public static void main(String[] args) 
+	{
+			launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception 
+	{
+		VBox root=new VBox();
+		Button 
+		
+	}
+	
+	
+
+}
