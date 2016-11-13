c404f7ca4b980b55e3267aa3256e276d3ecc3ee4
MihirMehta5695 <mihirm5695@gmail.com>
2016-11-13 16:42:39 +0530

    Dummy code to test JavaFX enviroment

--- application/Main.java
+++ application/Main.java
@@ -3,7 +3,11 @@ package application;
 
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,10 +20,28 @@ public class Main extends Application
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception 
+	public void start(Stage primarystage) throws Exception 
 	{
+		primarystage.setTitle("Advanced Task Killer");
+	
+		Label lable=new Label("Advanced Task Killer");
 		VBox root=new VBox();
-		Button 
+		
+		Button btn_exit =new Button();
+		btn_exit.setText("Exit");
+		btn_exit.setOnAction(new EventHandler<ActionEvent>()
+								{
+									@Override
+									public void handle(ActionEvent arg0) 
+									{
+										System.exit(0);
+									}
+								}
+							);
+		root.getChildren().addAll(new Label("Advanced Task Killer"),btn_exit);
+		Scene scene =new Scene(root,250,250);
+		primarystage.setScene(scene);
+		primarystage.show();
 		
 	}
 	
